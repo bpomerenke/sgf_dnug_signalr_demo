a5a775848b47a0a181b6bada1293262751d9b06b
Ben Pomerenke <ben.pomerenke@asolutions.com>
2015-08-22 12:11:59 -0500

    3 - simple login to associate name with ip - BP

--- DemoApp/DemoApp/Controllers/HomeWebController.cs
+++ DemoApp/DemoApp/Controllers/HomeWebController.cs
@@ -1,12 +1,56 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace DemoApp.Controllers
 {
     public class HomeWebController : Controller
     {
-        public ViewResult Index()
+        public static Dictionary<string,string> IpDictionary = new Dictionary<string, string>(); 
+        public ActionResult Index()
+        {
+            var ipAddress = Request.UserHostAddress;
+            var username = LookupUsername(ipAddress);
+            if (string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("Login");
+            }
+            var userInfo = new UserInfo { IpAddress = ipAddress,Username = username};
+            return View(userInfo);
+        }
+
+        [HttpGet]
+        public ViewResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult LoginUser(string name)
+        {
+            var ipAddress = Request.UserHostAddress;
+
+            if (IpDictionary.ContainsKey(ipAddress))
+            {
+                IpDictionary.Remove(ipAddress);
+            }
+            IpDictionary.Add(Request.UserHostAddress, name);
+            return RedirectToAction("Index");
+        }
+
+        private string LookupUsername(string ipAddress)
+        {
+            if (IpDictionary.ContainsKey(ipAddress))
+            {
+                return IpDictionary[ipAddress];
+            }
+
+            return null;
+        }
+    }
+
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string IpAddress { get; set; }
     }
 }
