6c1774a57458670ab8884eb12914ac6291ec7cb5
Ben Pomerenke <ben.pomerenke@asolutions.com>
2015-08-22 08:29:58 -0500

    1 - configured routes and added base info controller - BP

---
+++ DemoApp/DemoApp/Controllers/InfoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace DemoApp.Controllers
+{
+    public class InfoController : ApiController
+    {
+        public Info Get()
+        {
+            return new Info {AppName = "DemoApp"};
+        }
+    }
+
+    public class Info
+    {
+        public string AppName { get; set; }
+    }
+}
---
+++ DemoApp/DemoApp/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace DemoApp
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var config = GlobalConfiguration.Configuration;
+            config.Routes.MapHttpRoute(
+                name: "GenericController",
+                routeTemplate: "api/v1/{controller}/{id}",
+                defaults: new { controller = "Info", id = RouteParameter.Optional },
+                constraints: null);
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
