756ee5baa13b678238dcb1e933203a88a782e4cb
Ben Pomerenke <ben.pomerenke@asolutions.com>
2015-08-22 13:24:19 -0500

    5 - added dots - BP

---
+++ DemoApp/DemoApp/Controllers/DotController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+using DemoApp.Hubs;
+
+namespace DemoApp.Controllers
+{
+    public class DotController : ApiControllerWithHub<DotHub>
+    {
+        public HttpResponseMessage Post()
+        {
+            Hub.Clients.All.broadcastMessage("", "");
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
