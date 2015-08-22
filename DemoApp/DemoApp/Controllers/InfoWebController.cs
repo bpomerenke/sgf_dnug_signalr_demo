c95cffee102f86ddc52143d8f890cdb1736d6059
Ben Pomerenke <ben.pomerenke@asolutions.com>
2015-08-22 08:50:30 -0500

    2 - added mvc info page - BP

---
+++ DemoApp/DemoApp/Controllers/InfoWebController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class InfoWebController : Controller
+    {
+        public ViewResult Info()
+        {
+            var info = new Info {AppName = "DemoApp"};
+            return View(info);
+        }
+    }
+}
--- DemoApp/DemoApp/Global.asax.cs
+++ DemoApp/DemoApp/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace DemoApp
 {
@@ -9,12 +11,30 @@ namespace DemoApp
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            AreaRegistration.RegisterAllAreas();
+
             var config = GlobalConfiguration.Configuration;
             config.Routes.MapHttpRoute(
                 name: "GenericController",
                 routeTemplate: "api/v1/{controller}/{id}",
                 defaults: new { controller = "Info", id = RouteParameter.Optional },
                 constraints: null);
+
+
+            var routes = RouteTable.Routes;
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "InfoDefault",
+                url: "",
+                defaults: new { controller = "InfoWeb", action = "Info" }
+            );
+
+            routes.MapRoute(
+                name: "InfoWebPage",
+                url: "info",
+                defaults: new { controller = "InfoWeb", action = "Info" }
+            );
 
         }
 
