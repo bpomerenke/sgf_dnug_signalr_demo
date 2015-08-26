eef4c7b67405b538344a2b2dc24f67683e9f714a
Ben Pomerenke <ben.pomerenke@asolutions.com>
2015-08-25 22:56:36 -0500

    4 - added signalr - BP

---
+++ DemoApp/DemoApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using DemoApp;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace DemoApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
